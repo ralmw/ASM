03c6c31e97f5e199d8499ade89f10456026a481b
ralmw <61575357+ralmw@users.noreply.github.com>
2023-01-02 12:51:49 -0600

    New price function

---
+++ ASM julia/price_func.jl
@@ -0,0 +1,12 @@
+# La conclusión de todos mis esfuerzos es que lo haré de 2 maneras, dependiendo de un parametró decidiré si 
+# el precio es el que se encuentra justamente a la mitad de ambos agentes o se decide de manera proporcional 
+# a la riquza de ambos agentes de tal manera que priorize la opinión del más rico. 
+# La mágnifica ecuación resultante del trabajo del año pasado enterito es la siguiente:
+
+(s + x)*(P - Pt) + (E - xPt)(1+r)
+
+# La labor ahora está en calcular sencillamente un precio a partir de 2 agentes y usar la ecuación para
+# determinar la cantidad de acciones a comerciar. 
+
+
+# Recuperando mis códigos anteriores:
